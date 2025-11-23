c1dedac48d1a99fa97a93892daa855b6e7b5daa4
Daniel <95550573+O-DLAN@users.noreply.github.com>
2025-11-23 07:22:51 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 function testAPI() {
-fetch('/.netlify/functions/deepseek')
-.then(response => response.json())
-.then(data => {
-document.getElementById('result').innerHTML = JSON.stringify(data);
-})
-.catch(error => {
-document.getElementById('result').innerHTML = 'Error: ' + error.message;
-});
+  fetch('/.netlify/functions/deepseek')
+  .then(response => response.json())
+  .then(data => {
+    document.getElementById('result').innerHTML = data.reply;
+  })
+  .catch(error => {
+    document.getElementById('result').innerHTML = 'Error: ' + error.message;
+  });
 }
 document.getElementById('testApi').addEventListener('click', testAPI);
